6dcf0dd5cef62a3b7e037e9a8de4d2f4a81a9c19
Aleksey Kushnarev <scanavik2005@gmail.com>
2022-03-12 00:12:23 +0500

    add function fill_table

--- utils/misc/search_table.py
+++ utils/misc/search_table.py
@@ -1,28 +1,56 @@
 from PyQt5 import uic
 from PyQt5.QtWidgets import QApplication, QWidget, QTableWidgetItem
+from PyQt5 import QtGui
 import sys
 import sqlite3
+
+class EmptyLineEdit(Exception):
+    pass
+
 
 class TableWindow(QWidget):
     def __init__(self):
         super().__init__()
         uic.loadUi('../../static/ui/search_table.ui', self)  # Загружаем дизайн
         self.con = sqlite3.connect('../../static/database/db.db')
+        self.fill_table()
+        self.initUi()
+
+    def initUi(self):
+        self.find_btn.clicked.connect(self.find_in_table)
 
     def fill_table(self):
         cur = self.con.cursor()
         result = cur.execute(f"SELECT *"
                              f"FROM certificates").fetchall()
-        self.table.setRowCount(len(self.result))
-        self.table.setColumnCount(len(self.result[0]))
-
+        self.table.setRowCount(len(result))
+        self.table.setColumnCount(len(result[0]))
+        headers = ['№ п/п', '№ сертификата', 'наименование продукции', 'тип изделия', '№ заказа',
+                  'организация', 'потребитель', 'ФИО выдавшего сертификат']
+        for i, elem in enumerate(headers):
+            header = QTableWidgetItem(elem)
+            self.table.setHorizontalHeaderItem(i, header)
         # Заполнили таблицу полученными элементами
-        for i, elem in enumerate(self.result):
+        for i, elem in enumerate(result):
             for j, val in enumerate(elem):
-                self.tableWidget.setItem(i, j, QTableWidgetItem(str(val)))
+                self.table.setItem(i, j, QTableWidgetItem(str(val)))
+        self.table.horizontalHeader().setStretchLastSection(True)
 
     def find_in_table(self):
-        pass
+        print(1)
+        try:
+            request = self.search_line.text()
+            if not request:
+                raise EmptyLineEdit
+
+        except:
+            print(f'Поисковая строка пуста')
+
+        search_filter = self.search_criteria.currentText()
+
+
+
+
 
 sys.__excepthook__ = sys.__excepthook__
 
