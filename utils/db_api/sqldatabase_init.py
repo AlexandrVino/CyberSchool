7022398a587e37b617cfdd1e509497d5b99ce3ab
Anna <alikinaan8@yandex.ru>
2022-03-11 22:49:53 +0500

    create db

---
+++ utils/db_api/sqldatabase_init.py
@@ -0,0 +1,22 @@
+import sqlite3
+
+
+conn = sqlite3.connect('../../static/database/db.db')
+cur = conn.cursor()
+
+#Create certificates table and fill data
+cur.execute("""CREATE TABLE IF NOT EXISTS certificates(
+   id INTEGER PRIMARY KEY AUTOINCREMENT,
+   serial_number INT,
+   number_certificate INT,
+   product_name TEXT, 
+   product_type TEXT,
+   order_number INT,
+   consumer_organization TEXT,
+   shop_manufacturer TEXT,
+   full_name_of_the_certificate_issuer TEXT
+   );
+""")
+
+
+conn.commit()
