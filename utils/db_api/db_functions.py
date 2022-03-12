2e794b6d3bff3f01c0cf9c91a71051934ce6d185
AlexandrVino <64733647+ded-masay@users.noreply.github.com>
2022-03-12 16:17:03 +0500

    edit db_functions.py

--- utils/db_api/db_functions.py
+++ utils/db_api/db_functions.py
@@ -4,43 +4,59 @@ class ErrorCertificate(Exception):
     pass
 
 
-#give all columns value
-class DataBase():
-    def __init__(self):
-        self.db_name = "../../static/database/db.db"
+class DataBase:
+    """
+    Класс для работы с базой данных
+    """
 
-    def get_certificate_for_pdf(self, index_number):
+    def __init__(self, database_name: str):
+        self.db_name = database_name
+
+    def get_certificate_for_pdf(self, index_number: int) -> list:
+        """
+        :param index_number порядковй номер сертификата
+        :return: сертификат
+        """
+
         con = sqlite3.connect(self.db_name)
         cur = con.cursor()
 
         try:
             cur.execute(f"SELECT all FROM certificates WHERE serial_number ='{index_number}'")
-            return cur.fetchall()
+            return list(cur.fetchall())
         finally:
             cur.close()
             con.close()
 
-    def is_certificate_presents(self, index_number):
+    def is_certificate_presents(self, index_number: int) -> bool:
+        """
+        :param index_number порядковй номер сертификата
+        :return: str
+        """
+
+        # я бы возвращал bool))
+
         con = sqlite3.connect(self.db_name)
         cur = con.cursor()
-        message = "Успешно"
 
         try:
             cur.execute(f"INSERT OR IGNORE INTO certificates(index_number) "
                         f"VALUES({index_number});")
         except ErrorCertificate:
-            message = "Сертификат с таким номером уже существует, проверьте номер п/п"
+            return 1
         finally:
             con.commit()
             cur.close()
-            return message
+            return 0
 
-    #unpack dict and add certificate to the database
-    #тут пока заглушка
-    def add_certificate(self, dict_of_value):
+    def add_certificate(self, dict_of_value: dict) -> bool:
+        """
+        :param dict_of_value данные сертификата
+        :return: None
+        добавляет сертификат в бд
+        """
         con = sqlite3.connect(self.db_name)
         cur = con.cursor()
-        message = "Сертификат добавлен успешно"
 
         try:
             cur.execute(f"INSERT OR IGNORE INTO certificates() "
@@ -48,4 +64,4 @@ class DataBase():
         finally:
             con.commit()
             cur.close()
-            return message
+            return 1
