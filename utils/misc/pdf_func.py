d2ede72a6b739781aaa2bf7eca22f7e9b20f5917
AlexandrVino <64733647+ded-masay@users.noreply.github.com>
2022-03-12 19:16:29 +0500

    edit pdf structure

---
+++ utils/misc/pdf_func.py
@@ -0,0 +1,23 @@
+from docxtpl import DocxTemplate
+import os
+
+
+def create_word(data: dict):
+    # open template
+    doc = DocxTemplate("static/template/tpl.docx")
+
+    # format data
+    data["day"], data["month"], data["year"] = data["release_date"].split(".")
+    data["year"] = data["year"][2:]
+
+    # render & save docx file
+    doc.render(data)
+    doc.save("res.docx")
+
+    # call func printing file
+    print("print_file")
+
+    # delete docx file
+    path = os.path.join(os.path.abspath(os.path.dirname(__file__)), 'res.docx')
+    os.remove(path)
+
