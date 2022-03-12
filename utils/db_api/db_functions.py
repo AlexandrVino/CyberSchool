8efa92e5e62d22e46ae4f02953cba0dfc44313be
Anna <alikinaan8@yandex.ru>
2022-03-12 14:08:39 +0500

    fix get_certificate_for_pdf and other func

--- utils/db_api/db_functions.py
+++ utils/db_api/db_functions.py
@@ -5,26 +5,26 @@ class ErrorCertificate(Exception):
 
 
 #give all columns value
-def get_certificate_for_pdf(self, serial_number):
+def get_certificate_for_pdf(self, index_number):
     con = sqlite3.connect("../../static/database/db.db")
     cur = con.cursor()
 
     try:
-        cur.execute(f"SELECT all FROM certificates WHERE serial_number ='{serial_number}'")
+        cur.execute(f"SELECT all FROM certificates WHERE serial_number ='{index_number}'")
         return cur.fetchall()
     finally:
         cur.close()
         con.close()
 
 
-def is_certificate_presents(self, serial_number):
+def is_certificate_presents(self, index_number):
     con = sqlite3.connect("../../static/database/db.db")
     cur = con.cursor()
     message = "Успешно"
 
     try:
-        cur.execute(f"INSERT OR IGNORE INTO certificates(serial_number) "
-                    f"VALUES({serial_number});")
+        cur.execute(f"INSERT OR IGNORE INTO certificates(index_number) "
+                    f"VALUES({index_number});")
     except ErrorCertificate:
         message = "Сертификат с таким номером уже существует, проверьте номер п/п"
     finally:
